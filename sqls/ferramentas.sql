f338da96ba59617a41e2d180a6131d8c5baad870
Felipe F Garbim <felpefg.felp@gmail.com>
2024-08-06 11:00:25 -0300

    Create ferramentas.sql

---
+++ sqls/ferramentas.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `ferramentas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(110) DEFAULT NULL,
+  `obs` varchar(115) DEFAULT NULL,
+  `href` varchar(800) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
+
