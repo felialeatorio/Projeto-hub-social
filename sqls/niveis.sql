8a2b312bcc4562cc8c52c4ebbd67f19e930b961a
Felipe F Garbim <felpefg.felp@gmail.com>
2024-08-06 11:01:52 -0300

    Create niveis.sql

---
+++ sqls/niveis.sql
@@ -0,0 +1,7 @@
+
+CREATE TABLE `niveis` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nivel` varchar(45) DEFAULT NULL,
+  `nome` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
