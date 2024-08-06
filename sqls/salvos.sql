85d7f82f28092ed9ea6501d5d11d2dd2f4c438b7
Felipe F Garbim <felpefg.felp@gmail.com>
2024-08-06 11:02:40 -0300

    Create salvos.sql

---
+++ sqls/salvos.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE `salvos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_post` varchar(45) DEFAULT NULL,
+  `id_usuario` varchar(45) DEFAULT NULL,
+  `data` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=35 DEFAULT CHARSET=utf8;
